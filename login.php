a12252c0974782745313d454133ca247fc3ae9d2
Delbergue <delbergue.jonathan@hotmail.fr>
2013-02-19 11:54:35 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,1 +1,35 @@
-ggg
+<?php
+  if (isset($_POST['login']))
+  {
+    $login = $_POST['login'];
+  }
+  else
+  {
+    $login='';
+  }
+
+  if (isset($_POST['mdp']))
+    {
+      $mdp = $_POST['mdp'];
+   }
+   else
+    {
+      $mdp='';
+    }
+
+  include 'connexion.php';
+  $table = "NOMTABLE";
+  $requete = "SELECT login, mdp FROM ".$table." WHERE login=".$login;
+  $resultat = mysql_query($requete) or die('Erreur SQL !'.mysql_error());
+  $data = mysql_fetch_array($resultat);
+  if ($login==$data['login'] && $mdp==$data['mdp')
+  {
+    $log=true;
+  }
+  else
+  {
+    $log=false;
+  }
+/* NOMTABLE A REMPLIR + FICHIER A TESTER */
+/* Pour la connexion include ce fichier + tester la variable log (true = connecté /// false=erreur) */
+?>
