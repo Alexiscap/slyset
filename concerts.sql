c817dbd6649c3e8721df01bc033d77f4b341a0a9
Fenart <camille.fenart@gmail.com>
2013-04-28 23:23:09 +0200

    table concert mise a jours, re importer la

---
+++ concerts.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.8
+-- http://www.phpmyadmin.net
+--
+-- Client: localhost
+-- Généré le: Dim 28 Avril 2013 à 22:50
+-- Version du serveur: 5.6.10
+-- Version de PHP: 5.3.15
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données: `preprod-slyset`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `concerts`
+--
+
+CREATE TABLE IF NOT EXISTS `concerts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Artiste_id` int(11) NOT NULL,
+  `titre` varchar(255) DEFAULT NULL,
+  `lieu` varchar(45) DEFAULT NULL,
+  `date` timestamp NULL DEFAULT NULL,
+  `ville` varchar(20) NOT NULL DEFAULT 'NULL',
+  `snd_partie` varchar(40) NOT NULL DEFAULT 'NULL',
+  `website_salle` varchar(45) NOT NULL DEFAULT 'NULL',
+  `phone_salle` varchar(17) NOT NULL DEFAULT 'NULL',
+  `adress_salle` varchar(100) NOT NULL DEFAULT 'NULL',
+  `cp_ville` int(5) NOT NULL DEFAULT '0',
+  `prix` int(3) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`,`Artiste_id`),
+  KEY `fk_Concert_Artiste1_idx` (`Artiste_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Contenu de la table `concerts`
+--
+
+INSERT INTO `concerts` (`id`, `Artiste_id`, `titre`, `lieu`, `date`, `ville`, `snd_partie`, `website_salle`, `phone_salle`, `adress_salle`, `cp_ville`, `prix`) VALUES
+(1, 2, 'Bob Dylan', 'L''Aeronef', '2013-11-28 19:30:00', 'Lille', '', '', '', '', 0, 0),
+(2, 2, 'Beach House', 'Aeronef', '2013-04-04 22:00:00', '', '', '', '', '', 0, 0),
+(3, 3, 'Grizzly Bear', NULL, NULL, 'NULL', 'NULL', 'NULL', 'NULL', 'NULL', 0, 0),
+(4, 2, 'Grizlly Bear', 'Le Grand Mix', NULL, 'Lille', 'Lower Dens', 'NULL', 'NULL', 'NULL', 0, 15);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
