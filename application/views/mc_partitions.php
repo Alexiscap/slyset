0aab62b201ffce4bd0255eda3890cce6ec8cd613
Jonathan Delbergue <delbergue.jonathan@hotmail.fr>
2013-05-20 20:00:28 +0200

    ...

---
+++ application/views/mc_partitions.php
@@ -0,0 +1,354 @@
+<div id="contentAll">
+
+  <div id="breadcrumbs">
+    <ul>
+      <li><a href="#">Accueil</a></li>
+      <li><a href="#">Artistes</a></li>
+      <li><a href="#">Bob Dylan</a></li>
+      <li><a href="#">Photos & Vidéos</a></li>
+    </ul>
+  </div>
+
+  <div id="cover">
+    <div id="infos-cover">
+      <h2>Bob Dylan</h2>
+      <a href="#"><span class="button_left"></span><span class="button_center">Suivre</span><span class="button_right"></span></a>
+    </div>
+  </div>
+
+  <div id="stats-cover">
+    <div class="stats_cover_block">
+      <span class="stats_number">489</span>
+      <span class="stats_title">abonnés</span>
+    </div>
+
+    <div class="stats_cover_block">
+      <span class="stats_number">18</span>
+      <span class="stats_title">albums</span>
+    </div>
+
+    <div class="stats_cover_block">
+      <span class="stats_number">278</span>
+      <span class="stats_title">morceaux</span>
+    </div>
+  </div>
+  <div class="bt_noir">
+    <a href="#"><span class="bt_left"></span><span class="bt_middle">Ajouter des paroles</span><span class="bt_right"></span></a>
+  </div>
+  <div class="bt_noir">
+    <a href="#"><span class="bt_left"></span><span class="bt_middle">Ajouter un morceau</span><span class="bt_right"></span></a>
+  </div>
+
+  <div class="content">
+	<h2>Musique de Bob Dylan</h2>
+	<div class="a_la_une">
+		<img src="<?php echo img_url('musicien/album_top.jpg'); ?>"/>
+		<div class="player">
+			<a href="#"><img src="<?php echo img_url('musicien/player_top.png'); ?>"/></a>
+		</div>
+		<div class="infos">
+			<p class="title">Blonde on blonde</p>
+			<p class="annee_crea">1966</p>
+			<p><span>> </span><a href="#">Voir le livret d'album</a></p>
+			<p><span>> </span><a href="#">Voir les partitions</a></p>
+		</div>
+	</div>
+	<div class="top_partition">
+		<div>
+			<a href="#">
+				<img src="<?php echo img_url('musicien/player_top2.png'); ?>"/>
+				<p> Ecouter l'album</p>
+			</a>
+		</div>
+		<div class="liste_partitions">
+			<div class="en_tete">
+				<table>
+					<tr>
+						<td class="le_titre">Titre de la chanson</td>
+						<td class="paroles">Paroles</td>
+						<td class="partitions">Partitions</td>
+					</tr>
+				</table>
+			</div>
+			<div class="titres">
+				<table>
+					<tr>
+						<td class="le_titre">
+							<p> Rainy Day Women </p>
+						</td>
+						<td class="paroles"><a href="#">Voir</a>
+							<div class="miniat_titre">
+								<a href="#" class="edit"><span>edit</span></a>
+							</div>
+						</td>
+						<td class="partitions">
+							Acheter
+							<div class="miniat_titre">
+								<a href="#" class="cadis"><span>cadis</span></a>
+								<a href="#" class="edit"><span>edit</span></a>
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td class="le_titre">
+							<p> Hurricane </p>
+						</td>
+						<td class="paroles"><a href="#">Voir</a>
+							<div class="miniat_titre">
+								<a href="#" class="edit"><span>edit</span></a>
+							</div>
+						</td>
+						<td class="partitions">
+							Acheter
+							<div class="miniat_titre">
+								<a href="#" class="cadis"><span>cadis</span></a>
+								<a href="#" class="edit"><span>edit</span></a>
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td class="le_titre">
+							<p> I Want You </p>
+						</td>
+						<td class="paroles"><a href="#">Voir</a>
+							<div class="miniat_titre">
+								<a href="#" class="edit"><span>edit</span></a>
+							</div>
+						</td>
+						<td class="partitions">
+							Acheter
+							<div class="miniat_titre">
+								<a href="#" class="cadis"><span>cadis</span></a>
+								<a href="#" class="edit"><span>edit</span></a>
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td class="le_titre">
+							<p> I Want You </p>
+						</td>
+						<td class="paroles"><a href="#">Voir</a>
+							<div class="miniat_titre">
+								<a href="#" class="edit"><span>edit</span></a>
+							</div>
+						</td>
+						<td class="partitions">
+							Acheter
+							<div class="miniat_titre">
+								<a href="#" class="cadis"><span>cadis</span></a>
+								<a href="#" class="edit"><span>edit</span></a>
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td class="le_titre">
+							<p> I Want You </p>
+						</td>
+						<td class="paroles"><a href="#">Voir</a>
+							<div class="miniat_titre">
+								<a href="#" class="edit"><span>edit</span></a>
+							</div>
+						</td>
+						<td class="partitions">
+							Acheter
+							<div class="miniat_titre">
+								<a href="#" class="cadis"><span>cadis</span></a>
+								<a href="#" class="edit"><span>edit</span></a>
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td class="le_titre">
+							<p> I Want You </p>
+						</td>
+						<td class="paroles"><a href="#">Voir</a>
+							<div class="miniat_titre">
+								<a href="#" class="edit"><span>edit</span></a>
+							</div>
+						</td>
+						<td class="partitions">
+							Acheter
+							<div class="miniat_titre">
+								<a href="#" class="cadis"><span>cadis</span></a>
+								<a href="#" class="edit"><span>edit</span></a>
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td class="le_titre">
+							<p> I Want You </p>
+						</td>
+						<td class="paroles"><a href="#">Voir</a>
+							<div class="miniat_titre">
+								<a href="#" class="edit"><span>edit</span></a>
+							</div>
+						</td>
+						<td class="partitions">
+							Acheter
+							<div class="miniat_titre">
+								<a href="#" class="cadis"><span>cadis</span></a>
+								<a href="#" class="edit"><span>edit</span></a>
+							</div>
+						</td>
+					</tr>
+				</table>
+			</div>
+		</div>
+	</div>
+	<hr />
+	<div class="a_la_une">
+		<img src="<?php echo img_url('musicien/album_top.jpg'); ?>"/>
+		<div class="infos">
+			<p class="title">Tempest</p>
+			<p class="annee_crea">2012</p>
+			<p><span>> </span><a href="#">Ajouter le livret</a></p>
+		</div>
+	</div>
+	<div class="toutes_partitions">
+		<div>
+			<a href="#">
+				<img src="<?php echo img_url('musicien/player_top2.png'); ?>"/>
+				<p> Ecouter l'album</p>
+			</a>
+		</div>
+		<div class="liste_partitions">
+			<div class="en_tete">
+				<table>
+					<tr>
+						<td class="le_titre">Titre de la chanson</td>
+						<td class="paroles">Paroles</td>
+						<td class="partitions">Partitions</td>
+					</tr>
+				</table>
+			</div>
+			<div class="titres">
+				<table>
+					<tr>
+						<td class="le_titre">
+							<p> Rainy Day Women </p>
+						</td>
+						<td class="paroles"><a href="#">Voir</a>
+							<div class="miniat_titre">
+								<a href="#" class="edit"><span>edit</span></a>
+							</div>
+						</td>
+						<td class="partitions">
+							Acheter
+							<div class="miniat_titre">
+								<a href="#" class="cadis"><span>cadis</span></a>
+								<a href="#" class="edit"><span>edit</span></a>
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td class="le_titre">
+							<p> Hurricane </p>
+						</td>
+						<td class="paroles"><a href="#">Voir</a>
+							<div class="miniat_titre">
+								<a href="#" class="edit"><span>edit</span></a>
+							</div>
+						</td>
+						<td class="partitions">
+							Acheter
+							<div class="miniat_titre">
+								<a href="#" class="cadis"><span>cadis</span></a>
+								<a href="#" class="edit"><span>edit</span></a>
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td class="le_titre">
+							<p> I Want You </p>
+						</td>
+						<td class="paroles"><a href="#">Voir</a>
+							<div class="miniat_titre">
+								<a href="#" class="edit"><span>edit</span></a>
+							</div>
+						</td>
+						<td class="partitions">
+							Acheter
+							<div class="miniat_titre">
+								<a href="#" class="cadis"><span>cadis</span></a>
+								<a href="#" class="edit"><span>edit</span></a>
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td class="le_titre">
+							<p> I Want You </p>
+						</td>
+						<td class="paroles"><a href="#">Voir</a>
+							<div class="miniat_titre">
+								<a href="#" class="edit"><span>edit</span></a>
+							</div>
+						</td>
+						<td class="partitions">
+							Acheter
+							<div class="miniat_titre">
+								<a href="#" class="cadis"><span>cadis</span></a>
+								<a href="#" class="edit"><span>edit</span></a>
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td class="le_titre">
+							<p> I Want You </p>
+						</td>
+						<td class="paroles"><a href="#">Voir</a>
+							<div class="miniat_titre">
+								<a href="#" class="edit"><span>edit</span></a>
+							</div>
+						</td>
+						<td class="partitions">
+							Acheter
+							<div class="miniat_titre">
+								<a href="#" class="cadis"><span>cadis</span></a>
+								<a href="#" class="edit"><span>edit</span></a>
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td class="le_titre">
+							<p> I Want You </p>
+						</td>
+						<td class="paroles"><a href="#">Voir</a>
+							<div class="miniat_titre">
+								<a href="#" class="edit"><span>edit</span></a>
+							</div>
+						</td>
+						<td class="partitions">
+							Acheter
+							<div class="miniat_titre">
+								<a href="#" class="cadis"><span>cadis</span></a>
+								<a href="#" class="edit"><span>edit</span></a>
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td class="le_titre">
+							<p> I Want You </p>
+						</td>
+						<td class="paroles"><a href="#">Voir</a>
+							<div class="miniat_titre">
+								<a href="#" class="edit"><span>edit</span></a>
+							</div>
+						</td>
+						<td class="partitions">
+							Acheter
+							<div class="miniat_titre">
+								<a href="#" class="cadis"><span>cadis</span></a>
+								<a href="#" class="edit"><span>edit</span></a>
+							</div>
+						</td>
+					</tr>
+				</table>
+			</div>
+		</div>
+	</div>
+	</div>
+  </div>
+
+  <?php if(isset($sidebar_right)) echo $sidebar_right; ?>
+
+</div>
