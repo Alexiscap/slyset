9be676f01d8c15da9f3e0c303d5ada3e9b2f28df
Jonathan Delbergue <delbergue.jonathan@hotmail.fr>
2013-06-28 17:06:51 +0200

    Derniers retours (ou presque)

--- application/views/playlist/melo_playlist.php
+++ application/views/playlist/melo_playlist.php
@@ -53,82 +53,35 @@
 			</div>
 		</div>
 		<div class="clear"></div>
-		<div class="en_tete">
-			<table>
-				<tr>
-					<td class="check"><input type="checkbox"></td>
-					<td class="le_titre">Titre de la chanson</td>
-					<td class="artiste">Artiste</td>
-					<td class="album">Album</td>
-					<td class="duree">Durée</td>
-				</tr>
-			</table>
-		</div>
-		<div class="titres">
-			<table>
-				<tr>
-					<td class="check"><input type="checkbox"></td>
-					<td class="le_titre" onMouseOver="this.id='select';bt_edit();" onMouseOut="cache_edit();this.id='';">
-						<img src="<?php echo img_url('common/btn_play2.png'); ?>"/>
-						Rainy Day Women
-						<div class="miniat_titre">
-								<a href="#" class="cadis"><span>caddi</span></a>
-								<a href="#" class="coeur"><span>coeur</span></a>
-								<a href="#" class="cam"><span>cam</span></a>
-							</div>
-					</td>
-					<td class="artiste">Bob Dylan</td>
-					<td class="album">Blonde on blonde</td>
-					<td class="duree">4:19</td>
-				</tr>
-				<tr>
-					<td class="check"><input type="checkbox"></td>
-					<td class="le_titre" onMouseOver="this.id='select';bt_edit();" onMouseOut="cache_edit();this.id='';">
-						<img src="<?php echo img_url('common/btn_play2.png'); ?>"/>
-						Rainy Day Women
-						<div class="miniat_titre">
-								<a href="#" class="cadis"><span>caddi</span></a>
-								<a href="#" class="coeur"><span>coeur</span></a>
-								<a href="#" class="cam"><span>cam</span></a>
-							</div>
-					</td>
-					<td class="artiste">Bob Dylan</td>
-					<td class="album">Blonde on blonde</td>
-					<td class="duree">4:19</td>
-				</tr>
-				<tr>
-					<td class="check"><input type="checkbox"></td>
-					<td class="le_titre" onMouseOver="this.id='select';bt_edit();" onMouseOut="cache_edit();this.id='';">
-						<img src="<?php echo img_url('common/btn_play2.png'); ?>"/>
-						Rainy Day Women
-						<div class="miniat_titre">
-								<a href="#" class="cadis"><span>caddi</span></a>
-								<a href="#" class="coeur"><span>coeur</span></a>
-								<a href="#" class="cam"><span>cam</span></a>
-							</div>
-					</td>
-					<td class="artiste">Bob Dylan</td>
-					<td class="album">Blonde on blonde</td>
-					<td class="duree">4:19</td>
-				</tr>
-				<tr>
-					<td class="check"><input type="checkbox"></td>
-					<td class="le_titre" onMouseOver="this.id='select';bt_edit();" onMouseOut="cache_edit();this.id='';">
-						<img src="<?php echo img_url('common/btn_play2.png'); ?>"/>
-						Rainy Day Women
-						<div class="miniat_titre">
-								<a href="#" class="cadis"><span>caddi</span></a>
-								<a href="#" class="coeur"><span>coeur</span></a>
-								<a href="#" class="cam"><span>cam</span></a>
-							</div>
-					</td>
-					<td class="artiste">Bob Dylan</td>
-					<td class="album">Blonde on blonde</td>
-					<td class="duree">4:19</td>
-				</tr>
-			</table>
-			<input type="button" value="Acheter" class="cadis_pl">
-			<input type="button" value="Supprimer" class="bt_supp_playlist">
+		<div id="articles-tab">
+			<form action="http://127.0.0.1/slyset/index.php/admin_articles/delete_multi_article" method="post" accept-charset="utf-8">          
+				<table>
+					<tbody>
+						<tr class="tab-head odd row-color-2">
+							<th class="article-checkbox checkbox-style2"><input type="checkbox" name="article-all" value="all" class="check_all checkbox-article" id="article-all"><label for="article-all"><span></span></label></th>
+							<th class="article-title">Titre<span id="titre" class="filter filter-bottom"></span></th>
+							<th class="article-artiste">Artiste<span id="titre" class="filter filter-bottom"></span></th>
+							<th class="article-album">Album<span id="titre" class="filter filter-bottom"></span></th>
+							<th class="article-duree">Durée<span id="created" class="filter filter-bottom"></span></th>
+						</tr>
+							<tr class="even row-color-1">
+								<td class="article-checkbox checkbox-style2"><input type="checkbox" name="checkarticle[]" value="20" id="article-20" class="checkbox-article"><label for="article-20"><span></span></label></td>
+								<td class="article-title" onMouseOver="this.id='select';bt_edit();" onMouseOut="cache_edit();this.id='';">
+									<a href="#"><img src="<?php echo img_url('common/btn_play2.png'); ?>"/>
+									Rainy Winny
+									<div class="miniat_titre">
+										<a href="#" class="cadis"><span>caddi</span></a>
+										<a href="#" class="coeur"><span>coeur</span></a>
+										<a href="#" class="cam"><span>cam</span></a>
+									</div>
+								</td>
+								<td class="article-artiste">Tom Tom</td>
+								<td class="article-album">Et Nana</td>
+								<td class="article-duree">2:51</td>
+							</tr>
+					</tbody>
+				</table>
+			</form>
 		</div>
 	</div>
 	<hr />
@@ -151,172 +104,80 @@
 			</div>
 		</div>
 		<div class="clear"></div>
-		<div class="en_tete">
-			<table>
-				<tr>
-					<td class="check"><input type="checkbox"></td>
-					<td class="le_titre">Titre de la chanson</td>
-					<td class="artiste">Artiste</td>
-					<td class="album">Album</td>
-					<td class="duree">Durée</td>
-				</tr>
-			</table>
-		</div>
-		<div class="titres">
-			<table>
-				<tr>
-					<td class="check"><input type="checkbox"></td>
-					<td class="le_titre" onMouseOver="this.id='select';bt_edit();" onMouseOut="cache_edit();this.id='';">
-						<img src="<?php echo img_url('common/btn_play2.png'); ?>"/>
-						Rainy Day Women
-						<div class="miniat_titre">
-								<a href="#" class="cadis"><span>caddi</span></a>
-								<a href="#" class="coeur"><span>coeur</span></a>
-								<a href="#" class="cam"><span>cam</span></a>
-							</div>
-					</td>
-					<td class="artiste">Bob Dylan</td>
-					<td class="album">Blonde on blonde</td>
-					<td class="duree">4:19</td>
-				</tr>
-				<tr>
-					<td class="check"><input type="checkbox"></td>
-					<td class="le_titre" onMouseOver="this.id='select';bt_edit();" onMouseOut="cache_edit();this.id='';">
-						<img src="<?php echo img_url('common/btn_play2.png'); ?>"/>
-						Rainy Day Women
-						<div class="miniat_titre">
-								<a href="#" class="cadis"><span>caddi</span></a>
-								<a href="#" class="coeur"><span>coeur</span></a>
-								<a href="#" class="cam"><span>cam</span></a>
-							</div>
-					</td>
-					<td class="artiste">Bob Dylan</td>
-					<td class="album">Blonde on blonde</td>
-					<td class="duree">4:19</td>
-				</tr>
-				<tr>
-					<td class="check"><input type="checkbox"></td>
-					<td class="le_titre" onMouseOver="this.id='select';bt_edit();" onMouseOut="cache_edit();this.id='';">
-						<img src="<?php echo img_url('common/btn_play2.png'); ?>"/>
-						Rainy Day Women
-						<div class="miniat_titre">
-								<a href="#" class="cadis"><span>caddi</span></a>
-								<a href="#" class="coeur"><span>coeur</span></a>
-								<a href="#" class="cam"><span>cam</span></a>
-							</div>
-					</td>
-					<td class="artiste">Bob Dylan</td>
-					<td class="album">Blonde on blonde</td>
-					<td class="duree">4:19</td>
-				</tr>
-				<tr>
-					<td class="check"><input type="checkbox"></td>
-					<td class="le_titre" onMouseOver="this.id='select';bt_edit();" onMouseOut="cache_edit();this.id='';">
-						<img src="<?php echo img_url('common/btn_play2.png'); ?>"/>
-						Rainy Day Women
-						<div class="miniat_titre">
-								<a href="#" class="cadis"><span>caddi</span></a>
-								<a href="#" class="coeur"><span>coeur</span></a>
-								<a href="#" class="cam"><span>cam</span></a>
-							</div>
-					</td>
-					<td class="artiste">Bob Dylan</td>
-					<td class="album">Blonde on blonde</td>
-					<td class="duree">4:19</td>
-				</tr>
-				<tr>
-					<td class="check"><input type="checkbox"></td>
-					<td class="le_titre" onMouseOver="this.id='select';bt_edit();" onMouseOut="cache_edit();this.id='';">
-						<img src="<?php echo img_url('common/btn_play2.png'); ?>"/>
-						Rainy Day Women
-						<div class="miniat_titre">
-								<a href="#" class="cadis"><span>caddi</span></a>
-								<a href="#" class="coeur"><span>coeur</span></a>
-								<a href="#" class="cam"><span>cam</span></a>
-							</div>
-					</td>
-					<td class="artiste">Bob Dylan</td>
-					<td class="album">Blonde on blonde</td>
-					<td class="duree">4:19</td>
-				</tr>
-				<tr>
-					<td class="check"><input type="checkbox"></td>
-					<td class="le_titre" onMouseOver="this.id='select';bt_edit();" onMouseOut="cache_edit();this.id='';">
-						<img src="<?php echo img_url('common/btn_play2.png'); ?>"/>
-						Rainy Day Women
-						<div class="miniat_titre">
-								<a href="#" class="cadis"><span>caddi</span></a>
-								<a href="#" class="coeur"><span>coeur</span></a>
-								<a href="#" class="cam"><span>cam</span></a>
-							</div>
-					</td>
-					<td class="artiste">Bob Dylan</td>
-					<td class="album">Blonde on blonde</td>
-					<td class="duree">4:19</td>
-				</tr>
-				<tr>
-					<td class="check"><input type="checkbox"></td>
-					<td class="le_titre" onMouseOver="this.id='select';bt_edit();" onMouseOut="cache_edit();this.id='';">
-						<img src="<?php echo img_url('common/btn_play2.png'); ?>"/>
-						Rainy Day Women
-						<div class="miniat_titre">
-								<a href="#" class="cadis"><span>caddi</span></a>
-								<a href="#" class="coeur"><span>coeur</span></a>
-								<a href="#" class="cam"><span>cam</span></a>
-							</div>
-					</td>
-					<td class="artiste">Bob Dylan</td>
-					<td class="album">Blonde on blonde</td>
-					<td class="duree">4:19</td>
-				</tr>
-				<tr>
-					<td class="check"><input type="checkbox"></td>
-					<td class="le_titre" onMouseOver="this.id='select';bt_edit();" onMouseOut="cache_edit();this.id='';">
-						<img src="<?php echo img_url('common/btn_play2.png'); ?>"/>
-						Rainy Day Women
-						<div class="miniat_titre">
-								<a href="#" class="cadis"><span>caddi</span></a>
-								<a href="#" class="coeur"><span>coeur</span></a>
-								<a href="#" class="cam"><span>cam</span></a>
-							</div>
-					</td>
-					<td class="artiste">Bob Dylan</td>
-					<td class="album">Blonde on blonde</td>
-					<td class="duree">4:19</td>
-				</tr>
-				<tr>
-					<td class="check"><input type="checkbox"></td>
-					<td class="le_titre" onMouseOver="this.id='select';bt_edit();" onMouseOut="cache_edit();this.id='';">
-						<img src="<?php echo img_url('common/btn_play2.png'); ?>"/>
-						Rainy Day Women
-						<div class="miniat_titre">
-								<a href="#" class="cadis"><span>caddi</span></a>
-								<a href="#" class="coeur"><span>coeur</span></a>
-								<a href="#" class="cam"><span>cam</span></a>
-							</div>
-					</td>
-					<td class="artiste">Bob Dylan</td>
-					<td class="album">Blonde on blonde</td>
-					<td class="duree">4:19</td>
-				</tr>
-				<tr>
-					<td class="check"><input type="checkbox"></td>
-					<td class="le_titre" onMouseOver="this.id='select';bt_edit();" onMouseOut="cache_edit();this.id='';">
-						<img src="<?php echo img_url('common/btn_play2.png'); ?>"/>
-						Rainy Day Women
-						<div class="miniat_titre">
-								<a href="#" class="cadis"><span>caddi</span></a>
-								<a href="#" class="coeur"><span>coeur</span></a>
-								<a href="#" class="cam"><span>cam</span></a>
-							</div>
-					</td>
-					<td class="artiste">Bob Dylan</td>
-					<td class="album">Blonde on blonde</td>
-					<td class="duree">4:19</td>
-				</tr>
-			</table>
-			<input type="button" value="Acheter" class="cadis_pl">
-			<input type="button" value="Supprimer" class="bt_supp_playlist">
+		<div id="articles-tab">
+			<form action="http://127.0.0.1/slyset/index.php/admin_articles/delete_multi_article" method="post" accept-charset="utf-8">          
+				<table>
+					<tbody>
+						<tr class="tab-head odd row-color-2">
+							<th class="article-checkbox checkbox-style2"><input type="checkbox" name="article-all" value="all" class="check_all checkbox-article" id="article-all"><label for="article-all"><span></span></label></th>
+							<th class="article-title">Titre<span id="titre" class="filter filter-bottom"></span></th>
+							<th class="article-artiste">Artiste<span id="titre" class="filter filter-bottom"></span></th>
+							<th class="article-album">Album<span id="titre" class="filter filter-bottom"></span></th>
+							<th class="article-duree">Durée<span id="created" class="filter filter-bottom"></span></th>
+						</tr>
+							<tr class="even row-color-1">
+								<td class="article-checkbox checkbox-style2"><input type="checkbox" name="checkarticle[]" value="20" id="article-20" class="checkbox-article"><label for="article-20"><span></span></label></td>
+								<td class="article-title" onMouseOver="this.id='select';bt_edit();" onMouseOut="cache_edit();this.id='';">
+									<a href="#"><img src="<?php echo img_url('common/btn_play2.png'); ?>"/>
+									Rainy Winny
+									<div class="miniat_titre">
+										<a href="#" class="cadis"><span>caddi</span></a>
+										<a href="#" class="coeur"><span>coeur</span></a>
+										<a href="#" class="cam"><span>cam</span></a>
+									</div>
+								</td>
+								<td class="article-artiste">Tom Tom</td>
+								<td class="article-album">Et Nana</td>
+								<td class="article-duree">2:51</td>
+							</tr>
+							<tr class="even row-color-2">
+								<td class="article-checkbox checkbox-style2"><input type="checkbox" name="checkarticle[]" value="20" id="article-20" class="checkbox-article"><label for="article-20"><span></span></label></td>
+								<td class="article-title" onMouseOver="this.id='select';bt_edit();" onMouseOut="cache_edit();this.id='';">
+									<a href="#"><img src="<?php echo img_url('common/btn_play2.png'); ?>"/>
+									Rainy Winny
+									<div class="miniat_titre">
+										<a href="#" class="cadis"><span>caddi</span></a>
+										<a href="#" class="coeur"><span>coeur</span></a>
+										<a href="#" class="cam"><span>cam</span></a>
+									</div>
+								</td>
+								<td class="article-artiste">Tom Tom</td>
+								<td class="article-album">Et Nana</td>
+								<td class="article-duree">2:51</td>
+							</tr>
+							<tr class="even row-color-1">
+								<td class="article-checkbox checkbox-style2"><input type="checkbox" name="checkarticle[]" value="20" id="article-20" class="checkbox-article"><label for="article-20"><span></span></label></td>
+								<td class="article-title" onMouseOver="this.id='select';bt_edit();" onMouseOut="cache_edit();this.id='';">
+									<a href="#"><img src="<?php echo img_url('common/btn_play2.png'); ?>"/>
+									Rainy Winny
+									<div class="miniat_titre">
+										<a href="#" class="cadis"><span>caddi</span></a>
+										<a href="#" class="coeur"><span>coeur</span></a>
+										<a href="#" class="cam"><span>cam</span></a>
+									</div>
+								</td>
+								<td class="article-artiste">Tom Tom</td>
+								<td class="article-album">Et Nana</td>
+								<td class="article-duree">2:51</td>
+							</tr>
+							<tr class="even row-color-2">
+								<td class="article-checkbox checkbox-style2"><input type="checkbox" name="checkarticle[]" value="20" id="article-20" class="checkbox-article"><label for="article-20"><span></span></label></td>
+								<td class="article-title" onMouseOver="this.id='select';bt_edit();" onMouseOut="cache_edit();this.id='';">
+									<a href="#"><img src="<?php echo img_url('common/btn_play2.png'); ?>"/>
+									Rainy Winny
+									<div class="miniat_titre">
+										<a href="#" class="cadis"><span>caddi</span></a>
+										<a href="#" class="coeur"><span>coeur</span></a>
+										<a href="#" class="cam"><span>cam</span></a>
+									</div>
+								</td>
+								<td class="article-artiste">Tom Tom</td>
+								<td class="article-album">Et Nana</td>
+								<td class="article-duree">2:51</td>
+							</tr>
+					</tbody>
+				</table>
+			</form>
 		</div>
 	</div>
 	<hr />
