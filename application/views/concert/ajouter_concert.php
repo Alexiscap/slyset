8fa5fc03123be02a8b43d9ae2f03eeb8924720a7
Jonathan Delbergue <delbergue.jonathan@hotmail.fr>
2013-07-08 18:42:36 +0200

    pop in

--- application/views/concert/ajouter_concert.php
+++ application/views/concert/ajouter_concert.php
@@ -1,52 +1,87 @@
 <link rel="stylesheet" type="text/css" href="http://127.0.0.1/slyset/assets/css/slyset.css" media="screen" />
- 	 <div class="pop-in">
+ 	 <div class="pop-in_cent">
 
- 	<p> Ajouter un concert </p>
- 		 <img src="<?php echo img_url('musicien/pop_close.png'); ?>" alt="Fermer" />
-  <div class="content-pi"><?php
+ 	<span> Ajouter un concert </span>
+ 	
+  <div class="content-pi-cent"><?php
       //appel à la function "register" du controller "user"
       
 	  $user = $this->uri->segment(3);
-
+	?> <div class="elem_center"> <?php
       echo form_open('mc_concerts/ajouter_concert/'.$user);
-      		?><div class="label"><label>Artiste principal</label></div>
+      		?><div class="label"><label>Artistes</label></div>
       				<div class="champs">
 
 <?php
-      echo form_input('artiste', $this->session->userdata('login'),'placeholder="Ex : Beach House"')."<pre>";
-      echo form_error('artiste', '<div>', '</div>');
+	$artiste = array(
+              'class'        => 'artiste',
+              'name'          => 'artiste',
+	      'value'	=> $this->session->userdata('login'),
+	      'placeholder'	=> 'Ex : Beach House'
+            );
+      echo form_input($artiste);
+      echo form_error('artiste', '<div class="erreur-form">', '</div>');
 
-      echo form_input('snd_partie',set_value('snd_partie'),'placeholder="Ex : Lower Dens"')."<pre>";
+      echo form_input('snd_partie',set_value('snd_partie'),'placeholder="Ex : Lower Dens"');
       ?></div><div class="label"><label>Date</label></div>
       		<div class="champs">
 
 <?php
-      echo form_date('date_concert',set_value(''))."<pre>";
+	$date = array(
+              'class'        => 'date',
+              'name'          => 'date_concert',
+	      'value'	=> set_value('')
+            );
+      echo form_date($date);
       
       echo form_error('date_concert', '<div class="error-form" >', '</div>');
 ?></div><div class="label"><label>Heure</label></div><div class="champs">
 <?php
-      echo form_time('heure_concert',set_value(''))."<pre>";
+	$heure = array(
+              'class'        => 'heure',
+              'name'          => 'heure_concert',
+	      'value'	=> set_value('')
+            );
+      echo form_time($heure);
       ?>	</div>	
-<div class="label"><label>Salle</label></div><div class="champs">
+<div class="label"><label>Lieu</label></div><div class="champs">
 <?php
-      echo form_input('salle',set_value('salle'),'placeholder="Ex : Le Grand Mix"')."<pre>";
+	$salle = array(
+              'class'        => 'salle',
+              'name'          => 'salle',
+	      'value'	=> set_value('salle'),
+	      'placeholder'	=> 'Nom du bar, de la salle'
+            );
+      echo form_input($salle);
       echo form_error('salle', '<div class="error-form" >', '</div>');
-?></div><div class="label"><label>Ville</label></div><div class="champs">
+?>
 <?php
-      echo form_input('ville',set_value('ville'),'placeholder="Ex : Lille"')."<pre>";
+	$ville = array(
+              'class'        => 'ville',
+              'name'          => 'ville',
+	      'value'	=> set_value('ville'),
+	      'placeholder'	=> 'Ville'
+            );
+      echo form_input($ville);
       echo form_error('ville', '<div class="error-form" >', '</div>');
-	?></div><div class="label"><label>Prix</label></div><div class="champs_prix">
+	?></div><div class="label"><label>Prix</label></div><div class="champs">
 
 <?php
-      echo form_int('prix',set_value('prix'),'placeholder="Ex : 12"')."<pre>";
-      ?></div><?php
-      echo form_submit('submit','Ajouter un concert');
+	$prix = array(
+              'class'        => 'prix',
+              'name'          => 'prix',
+	      'value'	=> set_value('prix'),
+	      'placeholder'	=> 'Ex : 12'
+            );
+      echo form_int($prix);
+      ?>€</div><?php
+      echo form_submit('submit','Valider');
 
       echo form_close();
 
     ?>
+	</div>
 </div>
 
-</div>
+
 </div>
--- application/views/concert/modifier_concert.php
+++ application/views/concert/modifier_concert.php
@@ -1,26 +1,86 @@
+<link rel="stylesheet" type="text/css" href="http://127.0.0.1/slyset/assets/css/slyset.css" media="screen" />
+ 	 <div class="pop-in_cent">
+
+ 	<span> Modifier le concert </span>
+ 	
+  <div class="content-pi-cent">
+  
+  <?php
+
+//appel à la function "register" du controller "user"
+$user = $this->uri->segment(3);
+$concert = $this->uri->segment(4);
+$adresse = $this->uri->segment(5);
+
+echo form_open('mc_concerts/modifier_concert/' . $user . '/' . $concert . '/' . $adresse);
+
+?><div class="label"><label>Artistes</label></div>
+      				<div class="champs">
+
 <?php
-      //appel à la function "register" du controller "user"
-      $user = $this->uri->segment(3);
-	  $concert = $this->uri->segment(4);
-	  $adresse = $this->uri->segment(5);
+	$artiste = array(
+              'class'        => 'artiste',
+              'name'          => 'artiste',
+	      'value'	=> $this->session->userdata('login'),
+	      'placeholder'	=> 'Ex : Beach House'
+            );
+      echo form_input($artiste);
+      echo form_error('artiste', '<div class="erreur-form">', '</div>');
 
-      echo form_open('mc_concerts/modifier_concert/'.$user.'/'.$concert.'/'.$adresse);
-     
-      echo form_label('Artiste principal', 'artiste');
-      echo form_input('artiste',$info_concert[0]->{'titre'});
+      echo form_input('snd_partie',set_value('snd_partie'),'placeholder="Ex : Lower Dens"');
+      ?></div><div class="label"><label>Date</label></div>
+      		<div class="champs">
 
-      echo "2eme partie".form_input('snd_partie',$info_concert[0]->{'seconde_partie'})."<pre>";
-      echo "Date".form_date('date_concert',substr($info_concert[0]->{'date'},0,10))."<pre>";
+<?php
+	$date = array(
+              'class'        => 'date',
+              'name'          => 'date_concert',
+	      'value'	=> set_value('')
+            );
+      echo form_date($date);
       
+      echo form_error('date_concert', '<div class="error-form" >', '</div>');
+?></div><div class="label"><label>Heure</label></div><div class="champs">
+<?php
+	$heure = array(
+              'class'        => 'heure',
+              'name'          => 'heure_concert',
+	      'value'	=> set_value('')
+            );
+      echo form_time($heure);
+      ?>	</div>	
+<div class="label"><label>Lieu</label></div><div class="champs">
+<?php
+	$salle = array(
+              'class'        => 'salle',
+              'name'          => 'salle',
+	      'value'	=> set_value('salle'),
+	      'placeholder'	=> 'Nom du bar, de la salle'
+            );
+      echo form_input($salle);
+      echo form_error('salle', '<div class="error-form" >', '</div>');
+?>
+<?php
+	$ville = array(
+              'class'        => 'ville',
+              'name'          => 'ville',
+	      'value'	=> set_value('ville'),
+	      'placeholder'	=> 'Ville'
+            );
+      echo form_input($ville);
+      echo form_error('ville', '<div class="error-form" >', '</div>');
+	?></div><div class="label"><label>Prix</label></div><div class="champs">
 
-      echo "Heure".form_time('heure_concert',substr($info_concert[0]->{'date'},11,10))."<pre>";
-      echo "Salle".form_input('salle',$info_concert[0]->{'salle'})."<pre>";
-
-      echo "Ville".form_input('ville',$info_concert[0]->{'ville'})."<pre>";
-
-      echo "Prix".form_int('prix',$info_concert[0]->{'prix'})."<pre>";
-      
-      echo form_submit('submit','Modifier le concert');
+<?php
+	$prix = array(
+              'class'        => 'prix',
+              'name'          => 'prix',
+	      'value'	=> set_value('prix'),
+	      'placeholder'	=> 'Ex : 12'
+            );
+      echo form_int($prix);
+      ?>€</div><?php
+      echo form_submit('submit','Valider');
 
       echo form_close();
 
