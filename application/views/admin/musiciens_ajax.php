5f115260347b988b41bfc19f86d7f2a1ad22c574
Delbergue Jonathan <delbergue.jonathan@hotmail.fr>
2013-07-18 09:53:11 +0200

    Update musiciens_ajax.php

--- application/views/admin/musiciens_ajax.php
+++ application/views/admin/musiciens_ajax.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 $session_id = $this->session->userdata('uid');
 ?>
@@ -17,24 +16,4 @@ $session_id = $this->session->userdata('uid');
         <td class="compte-middle"><?php echo date('d-m-Y', strtotime(str_replace('-', '/', $musicien->created))) ?></td>
         <td class="compte-little"><?php echo $etat = ($musicien->suspendu == 0) ? 'Actif' : 'Suspendu'; ?></td>
     </tr>
-=======
-<?php
-$session_id = $this->session->userdata('uid');
-?>
-
-<?php foreach ($musiciens as $musicien): ?>
-    <tr>
-        <td class="compte-checkbox checkbox-style2"><input type="checkbox" name="checkcompte[]" value="<?php echo $musicien->id ?>" id="compte-<?php echo $musicien->id ?>" class="checkbox-compte" /><?php echo form_label('', "compte-$musicien->id") ?></td>
-        <td class="compte-little"><?php echo $musicien->id ?></td>
-        <td class="compte-title"><?php echo $musicien->login ?>
-            <div class="compte-actions">
-                <a href="<?php echo site_url('admin_musiciens/suspend/' . $musicien->id); ?>"><?php echo $etat_txt = ($musicien->suspendu == 0) ? 'Suspendre' : 'Activer'; ?></a>
-                | 
-                <a href="<?php echo site_url('admin_musiciens/delete/' . $musicien->id); ?>">Supprimer</a>
-            </div>
-        </td>
-        <td class="compte-middle"><?php echo date('d-m-Y', strtotime(str_replace('-', '/', $musicien->created))) ?></td>
-        <td class="compte-little"><?php echo $etat = ($musicien->suspendu == 0) ? 'Actif' : 'Suspendu'; ?></td>
-    </tr>
->>>>>>> 288ecf8... correction de mes bugs
 <?php endforeach; ?>
