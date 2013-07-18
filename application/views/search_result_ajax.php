474f55da455233c57321344eac31b0827f43e191
Delbergue Jonathan <delbergue.jonathan@hotmail.fr>
2013-07-18 09:56:21 +0200

    Update search_result_ajax.php

--- application/views/search_result_ajax.php
+++ application/views/search_result_ajax.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 $session_id = $this->session->userdata('uid');
 ?>
@@ -19,26 +18,4 @@ $session_id = $this->session->userdata('uid');
         <td class="result-middle"><?php echo $style_joue = (!empty($row['style_joue'])) ? $row['style_joue'] : '-'; ?></td>
         <td class="result-middle"><?php echo $type = ($row['type'] == 2) ? 'Musicien' : 'Mélomane'; ?></td>
     </tr>
-=======
-<?php
-$session_id = $this->session->userdata('uid');
-?>
-
-<?php foreach ($results as $row): ?>
-    <tr class="search_result">
-        <td class="result-photo">
-            <a href="<?php echo site_url('my-wall/' . $row['id']); ?>">
-                <img src="<?php echo $thumb = (!empty($row['thumb'])) ? files('profiles/' . $row['thumb']) : img_url('sidebar-right/default-photo-profil.png'); ?>" height="38" alt="Photo Profil" />
-            </a>
-        </td>
-        <td class="result-title">
-            <a href="<?php echo site_url('my-wall/' . $row['id']); ?>">
-                <?php echo $row['login']; ?>
-            </a>
-        </td>
-        <td class="result-middle"><?php echo $style_ecoute = (!empty($row['style_ecoute'])) ? $row['style_ecoute'] : '-'; ?></td>
-        <td class="result-middle"><?php echo $style_joue = (!empty($row['style_joue'])) ? $row['style_joue'] : '-'; ?></td>
-        <td class="result-middle"><?php echo $type = ($row['type'] == 2) ? 'Musicien' : 'Mélomane'; ?></td>
-    </tr>
->>>>>>> 288ecf8... correction de mes bugs
 <?php endforeach; ?>
