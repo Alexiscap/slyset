1e07749c5283cc64cd4ed583f21f0c971a5c29bb
Delbergue Jonathan <delbergue.jonathan@hotmail.fr>
2013-07-18 09:34:50 +0200

    Update email.php

--- application/config/email.php
+++ application/config/email.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 if (!defined('BASEPATH'))
@@ -14,21 +13,5 @@ $config['validate'] = 'FALSE';
 $config['smtp_host'] = 'localhost';
 $config['mailtype'] = 'html';
 $config['crlf'] = "\r\n";        // CHANGED FROM DEFAULTS
-=======
-<?php
 
-if (!defined('BASEPATH'))
-    exit('No direct script access allowed');
-/*
-  | -------------------------------------------------------------------------
-  | Configuration EMAIL
-  | -------------------------------------------------------------------------
- */
-
-$config['protocol'] = 'smtp';
-$config['validate'] = 'FALSE';
-$config['smtp_host'] = 'localhost';
-$config['mailtype'] = 'html';
-$config['crlf'] = "\r\n";        // CHANGED FROM DEFAULTS
->>>>>>> 288ecf8... correction de mes bugs
 $config['newline'] = "\r\n";        // CHANGED FROM DEFAULTS
