f9a181d6e7fc60497378aed874ea67fca1bfc1b0
Delbergue Jonathan <delbergue.jonathan@hotmail.fr>
2013-08-27 15:37:44 +0200

    Update ajouter_photos.php

--- application/views/photos/ajouter_photos.php
+++ application/views/photos/ajouter_photos.php
@@ -19,7 +19,7 @@
             <?php echo form_open_multipart('pop_in_general/do_upload/' . $user); ?>
 
             <div class="label"><label>Votre photo</label></div>
-            <div class="champs">
+             <div class="champs">
                 <?php
                 $data = array(
                     'name' => 'photo_up',
@@ -27,8 +27,9 @@
                     'class' => 'photo_up',
                     'value' => 'Choisir une photo'
                 );
-                ?><div class="bt_noir">
+                ?><div class="bt_noir bt_photo_up">
                 <?php echo form_upload($data); ?>
+				<span class="upload_photo">aucun fichier choisi</span>
                 </div>
             </div>
 
