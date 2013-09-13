80001c46df850e8a93d192d9c978e2adde0c455a
Delbergue Jonathan <delbergue.jonathan@hotmail.fr>
2013-09-13 09:45:45 +0200

    Update mc_photos.php

--- application/views/photos/mc_photos.php
+++ application/views/photos/mc_photos.php
@@ -101,7 +101,7 @@ $loger = $this->session->userdata('logged_in');
 
                     <div class="bord_photo">
                         <a href="javascript:void(0);">
-                            <p><?php if ($cpt_comment == 0) print "0 commentaire"; if ($cpt_comment == 1) print "1 commentaire"; if ($cpt_comment > 1) print $cpt_comment . "commentaires" ?></p></a>
+                            <p><?php if ($cpt_comment == 0) print "0 commentaire"; if ($cpt_comment == 1) print "1 commentaire"; if ($cpt_comment > 1) print $cpt_comment . " commentaires" ?></p></a>
                         <?php
                         $count = substr_count($all_photo_like, $media_user_result_unit->id . '/');
                         if ($count >= 1) {
@@ -236,7 +236,7 @@ $loger = $this->session->userdata('logged_in');
 					</div>
                     
                     <div class="bord_photo">
-                        <a href="javascript:void(0);"><p><?php if ($cpt_comment == 0) echo "0 commentaire"; if ($cpt_comment == 1) echo "1 commentaire"; if ($cpt_comment > 1) echo $cpt_comment . "commentaires"; ?></p></a>
+                        <a href="javascript:void(0);"><p><?php if ($cpt_comment == 0) echo "0 commentaire"; if ($cpt_comment == 1) echo "1 commentaire"; if ($cpt_comment > 1) echo $cpt_comment . " commentaires"; ?></p></a>
         <?php
         $count = substr_count($all_album_like, $media_user_result_unit->file_name . '/');
         if ($count >= 1) {
@@ -320,7 +320,7 @@ $loger = $this->session->userdata('logged_in');
                     ?>
 				</div>
                     <div class="bord_photo">
-                        <a href="javascript:void(0);"><p><?php if ($cpt_comment == 0) print "0 commentaire"; if ($cpt_comment == 1) print "1 commentaire"; if ($cpt_comment > 1) print $cpt_comment . "commentaires"; ?></p>
+                        <a href="javascript:void(0);"><p><?php if ($cpt_comment == 0) print "0 commentaire"; if ($cpt_comment == 1) print "1 commentaire"; if ($cpt_comment > 1) print $cpt_comment . " commentaires"; ?></p>
                         </a>
                     <?php
                     $count = substr_count($all_video_like, $media_user_result_unit->id . '/');
