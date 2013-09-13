8e7c1dd1b2baafe34409dcd81752becf07a9d05f
Delbergue Jonathan <delbergue.jonathan@hotmail.fr>
2013-09-13 10:10:18 +0200

    Update photos_success.php

--- application/views/photos/photos_success.php
+++ application/views/photos/photos_success.php
@@ -2,18 +2,6 @@
  	 <div class="pop-in_cent confirmation">
 		<span></span>
 	  <div class="content-pi-cent content-pi-cent_success">
-		<?php 
-				//appel à la function "register" du controller "user"
-				$user = $this->uri->segment(3);
-				$media = $this->uri->segment(4);
-				$type = $this->uri->segment(5);
-
-		if($type==1)
-			{echo "<p class='confirm_sup'>Votre photo a bien été ajoutée !</p>" ;}
-		if($type==2)
-			{echo "<p class='confirm_sup'>Votre album a bien été modifié !</p>" ;}
-		if($type==3)
-			{echo "<p class='confirm_sup'>Votre vidéo a bien été ajoutée !</p>" ;}
-	?>
+		<p class='confirm_sup'>Votre photo a bien été ajoutée !</p>
 	  </div>
 	 </div>
